b45da2aab79aff1781140313b266a46b9ad21291
Vijay Gaikwad <vijaygkd@gmail.com>
2018-02-18 18:27:56 -0800

    change output file name

--- simulator/generate_data.py
+++ simulator/generate_data.py
@@ -2,7 +2,7 @@ from simulator import simulate_all_boards
 
 data = simulate_all_boards()
 
-file_path = "data.csv"
+file_path = "raw_data.csv"
 print("Writing data to file:", file_path)
 with open(file_path, mode='w', encoding='utf-8') as output_file:
     output_file.write('\n'.join(data))
